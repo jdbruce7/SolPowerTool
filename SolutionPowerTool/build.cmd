2d0f8b1767cf3ef17f0a113074c919bb5ade0f02
jdbruce_cp <SND\jdbruce_cp>
2011-11-01 18:50:07 +0000

    Add build environment checks to build.cmd

--- SolutionPowerTool/build.cmd
+++ SolutionPowerTool/build.cmd
@@ -1,5 +1,27 @@
+@ECHO OFF
 setlocal
-call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+
+SET ScriptsDir=%~dp0\Scripts
+SET BinDir=.\Bin
+SET Pause=true
+SET Register=true
+
+IF "%1"=="/?" GOTO HELP
+
+IF DEFINED DevEnvDir GOTO OPTIONS
+
+IF NOT DEFINED VS100COMNTOOLS GOTO VSNOTFOUND
+
+ECHO.
+ECHO ------------------------------------------
+ECHO Setting the build environment
+ECHO ------------------------------------------
+ECHO.
+CALL "%VS100COMNTOOLS%\vsvars32.bat" > NUL 
+IF ERRORLEVEL 1 GOTO ERROR
+
+
+:OPTIONS
 
 msbuild /t:build /p:Configuration=Debug SolPowerTool\SolPowerTool.csproj
 msbuild /t:build /p:Configuration=Release SolPowerTool\SolPowerTool.csproj
