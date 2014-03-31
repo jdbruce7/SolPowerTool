f9a98dab257f3d7f003b6bf69bdc5480e52fcb7c
jdbruce_cp <SND\jdbruce_cp>
2014-03-31 20:49:17 +0000

    Bumped version

--- SolutionPowerTool/SolPowerTool/Properties/AssemblyInfo.cs
+++ SolutionPowerTool/SolPowerTool/Properties/AssemblyInfo.cs
@@ -52,8 +52,8 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
 
 // TODO: Cleanup ControlTabs panel in ShellView.xaml.
 
